ec76ae5bc645a7c24ed1f3fa29b1efaeeacf1666
Eliot Courtney <edgeworth.dev@gmail.com>
2022-03-26 16:27:54 +0900

    Initial commit.

---
+++ src/geom/bounds.rs
@@ -0,0 +1,27 @@
+use crate::primitive::point::Pt;
+use crate::primitive::rect::Rt;
+
+#[must_use]
+pub fn pt_cloud_bounds(pts: &[Pt]) -> Rt {
+    if pts.is_empty() {
+        Rt::default()
+    } else {
+        let mut bl = pts[0];
+        let mut tr = pts[0];
+        for pt in pts {
+            bl.x = bl.x.min(pt.x);
+            bl.y = bl.y.min(pt.y);
+            tr.x = tr.x.max(pt.x);
+            tr.y = tr.y.max(pt.y);
+        }
+        Rt::enclosing(bl, tr)
+    }
+}
+
+pub fn rt_cloud_bounds<I: IntoIterator<Item = Rt>>(rts: I) -> Rt {
+    let mut b = Rt::empty();
+    for r in rts {
+        b = b.united(&r);
+    }
+    b
+}
---
+++ src/geom/mod.rs
@@ -0,0 +1,7 @@
+pub mod bounds;
+pub mod contains;
+pub mod convex;
+pub mod distance;
+pub mod intersects;
+pub mod math;
+pub mod qt;
